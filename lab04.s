9ce027d5ad769f92d371509a31e3fa70d815e009
cs05457 <cs05457@uoi.gr>
2024-11-09 19:20:30 +0200

    Completed Lab04

--- lab04.s
+++ lab04.s
@@ -30,6 +30,30 @@ str_ge:
 # Write the subroutine code here
 #  You may move jr ra   if you wish.
 #---------
+    lbu t0,0(a0)
+    lbu t1,0(a1)
+
+    
+loop:    
+    beq t0,zero,return0
+    beq t1,zero,return1
+    beq t0,t1,skip
+    blt t0,t1,return0
+    j return1
+    
+    
+skip:
+    addi a0,a0,1
+    addi a1,a1,1
+    lbu t0,0(a0)
+    lbu t1,0(a1)
+    j loop   
+return0:
+    add a0,zero,zero
+    j end
+return1:
+    addi a0,zero,1
+end:
             jr   ra
  
 # ----------------------------------------------------------------------------
@@ -46,4 +70,33 @@ recCheck:
 # Write the subroutine code here
 #  You may move jr ra   if you wish.
 #---------
-            jr   ra
+    addi sp,sp,-12
+    sw ra,8(sp)
+    sw a1,4(sp)
+    sw a0,0(sp)
+    
+    slti t0,a1,2
+    beq t0,zero,skipReturn
+    
+    addi a0,zero,1
+    addi sp,sp,12
+    jr   ra
+skipReturn:
+    lw a1,0(a0)
+    lw a0,4(a0)
+    jal ra,str_ge
+    bne a0,zero,then
+    addi ,a0,zero,0
+    addi sp,sp,8
+    lw ra,0(sp)
+    jr ra
+then:
+    lw a0,0(sp)
+    lw a1,4(sp)
+    addi sp,sp,8
+    addi a1,a1,-1
+    addi a0,a0,4
+    jal ra,recCheck
+    addi sp,sp,4
+    lw ra,0(sp)
+    jr ra
